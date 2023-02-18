07ac285703a3ec4f5fe3730f9d19b67c89a77670
vanya_ttoneva@students.softuni.bg <vanya_ttoneva@students.softuni.bg>
2023-02-18 14:23:44 +0200

    Add project files.

---
+++ ApocalipsePreparation/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace ApocalipsePreparation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int patchResourses = 30;
+            const int bangdageResousres = 40;
+            const int medKitResourse = 100;
+
+
+            Queue<int> textile = new(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Stack<int> medicaments = new(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Dictionary<string, int> medKit = new();
+            bool areOver = false;
+            while (!areOver)
+            {
+                int currentMedicament = medicaments.Pop();
+                int currentHealtItem = textile.Dequeue() * currentMedicament;
+                int remainingMedicament = 0;
+                string currentHealingItemName = String.Empty;
+                if (currentHealtItem >= patchResourses && currentHealtItem < bangdageResousres)
+                {
+                    remainingMedicament = currentHealtItem - patchResourses;
+                    currentHealingItemName = "Patch";
+
+                }
+                else if (currentHealtItem >= bangdageResousres && currentHealtItem < medKitResourse)
+                {
+                    remainingMedicament = currentHealtItem - bangdageResousres;
+                    currentHealingItemName = "Bandage";
+                }
+                else if (currentHealtItem >= medKitResourse)
+                {
+                    remainingMedicament = currentHealtItem - medKitResourse;
+                    currentHealingItemName = "MedKit";
+                }
+
+                if (currentHealingItemName != "")
+                {
+                    if (remainingMedicament > 0)
+                    {
+                        medicaments.Push(remainingMedicament);
+                        if (!medKit.ContainsKey(currentHealingItemName))
+                        {
+                            medKit.Add(currentHealingItemName, 0);
+                        }
+                        medKit[currentHealingItemName]++;
+                    }
+                }
+                else
+                {
+                    medicaments.Push(currentMedicament + 10);
+                }
+                if (textile.Count == 0 || medicaments.Count == 0)
+                {
+                    areOver = true;
+                }
+            }
+
+            if (textile.Count == 0 && medicaments.Count == 0)
+            {
+                Console.WriteLine("Textiles and medicaments are both empty.");
+            }
+            else if (textile.Count > 0 && medicaments.Count == 0)
+            {
+                Console.WriteLine("Medicaments are empty.");
+            }
+            else if (textile.Count == 0 && medicaments.Count > 0)
+            {
+                Console.WriteLine("Textiles are empty.");
+            }
+            foreach(var med in medKit.OrderByDescending(x => x.Value).ThenBy(x=> x.Key))
+            {
+                Console.WriteLine($"{med.Key} - {med.Value}");
+            }
+
+            if (medicaments.Any())
+            {
+                Console.WriteLine($"Medicaments left: {String.Join(", ", medicaments)}");
+            }
+            if (textile.Any())
+            {
+                Console.WriteLine($"Textiles left: {String.Join(", ", textile)}");
+            }
+    }
+}
+}
---
+++ BlindManBuff/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace BlindManBuff
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimmension = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            char[][] playground = new char[dimmension[0]][];
+            int touchedOpponents = 0;
+            int movesMade = 0;
+            int playerPositionRow = 0;
+            int playerPositionCol = 0;
+            for(int i = 0; i < playground.GetLength(0); i++)
+            {
+                char[] currentRowInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                char[] currentRow = new char[dimmension[1]];
+                for(int j = 0; j < dimmension[1]; j++)
+                {
+                    currentRow[j] = currentRowInput[j];
+                }
+                playground[i] = currentRow;
+            }
+
+            for (int i = 0; i < playground.GetLength(0); i++) 
+            {
+                bool isFound = false;
+                for(int j = 0; j < playground[i].Length; j++)
+                {
+                    if (playground[i][j] == 'B')
+                    {
+                        playground[i][j] = '-';
+                        playerPositionRow = i;
+                        playerPositionCol = j;
+                        isFound = true;
+                        break;
+                    }
+                }
+                if (isFound) break;
+            }
+
+            string command = Console.ReadLine();
+            while(command != "Finish")
+            {
+                int[] moveResult = Play(command, playground, movesMade, touchedOpponents, playerPositionRow, playerPositionCol);
+                if(moveResult != null && touchedOpponents < 3)
+                {
+                    playerPositionRow = moveResult[0];
+                    playerPositionCol = moveResult[1];
+                    movesMade = moveResult[2];
+                    touchedOpponents = moveResult[3];
+                }
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine("Game over!");
+            Console.WriteLine($"Touched opponents: {touchedOpponents} Moves made: {movesMade}");
+        }
+
+
+        public static int[] Play(string move, char[][] playground, int movesMade, int touchedOpponents, int playerPositionRow, int playerPositionCol)
+        {
+
+            int newRow = playerPositionRow;
+            int newCol = playerPositionCol; 
+            switch (move) 
+            {
+                case "up":
+                    newRow--;
+                    break;
+                case "down":
+                    newRow++;
+                    break;
+                case "right":
+                    newCol++;
+                    break;
+                case "left":
+                    newCol--;
+                    break;
+
+            }
+            if(newRow < 0 ||newRow >= playground.GetLength(0) ||
+                newCol < 0 || newCol >= playground[playerPositionRow].Length)
+            {
+                return null;
+            }
+            if (playground[newRow][newCol] == 'O')
+            {
+                return null;
+            }
+
+            playerPositionRow = newRow;
+            playerPositionCol = newCol;
+            movesMade++;
+            if (playground[playerPositionRow][playerPositionCol] == 'P')
+            {
+                touchedOpponents++;
+                playground[playerPositionRow][playerPositionCol] = '-';
+            }
+            return new int[] { playerPositionRow, playerPositionCol, movesMade, touchedOpponents };
+        }
+
+    }
+
+    
+}
+
